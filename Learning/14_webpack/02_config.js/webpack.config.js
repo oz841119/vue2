3ff8686d23883d6d44ec0e07d2f732d5ed7fed91
oz841119 <oz841119@gmail.com>
2021-07-20 23:07:13 +0800

    webpack包

--- Learning/14_webpack/02_config.js/webpack.config.js
+++ Learning/14_webpack/02_config.js/webpack.config.js
@@ -5,5 +5,21 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader',
+                    loader: 'css-loader',
+                    loader: 'less-loader'
+
+                }]
+            }
+        ]
     }
 }
