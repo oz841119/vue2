7ef3f4ebaa69f7f908cef40491604855953eba4c
oz841119 <oz841119@gmail.com>
2021-07-27 02:43:58 +0800

    0727 tabbar製作

---
+++ Learning/19_tabbar基礎實現/tabbar/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home-active.png" alt="">
+        <div slot="item-font">首頁</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/category.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category-active.png" alt="">
+        <div slot="item-font">分類</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/cart.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/cart-active.png" alt="">
+        <div slot="item-font">購物車</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/account.png" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/account-active.png" alt="">
+        <div slot="item-font">我的</div>
+      </tab-bar-item>
+    </tab-bar> <!-- 3.使用組件 -->
+  </div>
+</template>
+
+<script>
+// import { component } from 'vue/types/umd'
+  import TabBar from "./components/tabbar/TabBar" // 1.導入組件
+  import TabBarItem from "./components/tabbar/TabBarItem"
+  export default {
+    name: 'App',
+    components: {
+      TabBar,  // 2.註冊組件
+      TabBarItem
+    }
+  }
+
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
---
+++ Learning/19_tabbar基礎實現/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <!-- 一般不建議在插槽使用v-if或任何樣式指令 所以外面包一層div使用 -->
+    <div class="tab-bar-item" :class="{active: isActive}">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <slot name="item-font"></slot>
+        <!-- <img src="../../assets/img/tabbar/home.png">
+        <div>首頁</div> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabBarItem',
+    data(){
+      return {
+        isActive: true
+      }
+    }
+}
+</script>
+
+<style>
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0px -1px 1px rgba(0,0,0,.1);
+  }
+
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;   /* 補充:一般tabbar設計都是49px的高度 */ 
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin: 3px 0 0;
+    vertical-align: middle;
+  }
+
+  .active {
+    color: red;
+  }
+</style>
